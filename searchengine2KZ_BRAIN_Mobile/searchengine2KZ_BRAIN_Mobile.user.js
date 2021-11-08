b45415a6d4415a45a8f5da70e96d8bca2630757d
gm_fox <49097489+PC-CNT@users.noreply.github.com>
2021-11-08 10:27:59 +0900

    Update searchengine2KZ_BRAIN_Mobile.user.js

--- searchengine2KZ_BRAIN_Mobile/searchengine2KZ_BRAIN_Mobile.user.js
+++ searchengine2KZ_BRAIN_Mobile/searchengine2KZ_BRAIN_Mobile.user.js
@@ -8,16 +8,16 @@
 // ==/UserScript==
 
 window.onload = function(){
+    console.log("===START UserscriptWorks/searchengine2KZ_BRAIN_Mobile===");
     const KZ_url = "http://servermobile.net/index.php?_kzm_u=";
 
     let div_id_search = document.getElementById("search");
     div_id_search.querySelectorAll(".g").forEach(function(value) {
         let a_tag_main = value.querySelector("a");
-        // console.log(a_tag_main);
         let url_source = a_tag_main.getAttribute("href");
         a_tag_main.setAttribute("href", KZ_url + url_source);
         // console.log(url_source);
-        // console.log("test");
         // 検索結果の数だけ繰り返す
     });
+    console.log("===END UserscriptWorks/searchengine2KZ_BRAIN_Mobile===");
 }
