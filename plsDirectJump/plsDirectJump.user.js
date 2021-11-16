7820128513a04657e12bb8695aa7cfb36eb6186c
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2021-11-16 15:15:51 +0900

    Update plsDirectJump.user.js

--- plsDirectJump/plsDirectJump.user.js
+++ plsDirectJump/plsDirectJump.user.js
@@ -2,7 +2,7 @@
 // @name            plsDirectJump
 // @namespace       https://github.com/PC-CNT/UserscriptWorks/
 // @version         0.0.1
-// @description:ja  2ch.netやFC2 Wikiなどのサイト内リンクからクッションページを削除して直接飛ぶようにするスクリプトです。
+// @description:ja  2ch.netやFC2 Wikiなどのサイト内リンクからクッションページを削除して直接飛ぶようにするスクリプト（の予定）です。
 // @auther          PC-CNT
 // @updateURL       https://raw.githubusercontent.com/PC-CNT/UserscriptWorks/main/plsDirectJump/plsDirectJump.js
 // @include         /^https?:\/\/*
