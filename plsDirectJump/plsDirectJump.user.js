96d8ccfe37c8b5cd346bbd5931a1f6288ccc548f
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2021-11-16 15:02:06 +0900

    [fix] some code

--- plsDirectJump/plsDirectJump.user.js
+++ plsDirectJump/plsDirectJump.user.js
@@ -1,24 +1,24 @@
 // ==UserScript==
-// @name          plsDirectJump
-// @namespace     https://github.com/PC-CNT/UserscriptWorks/
-// @version       0.0.1
-// @description   2ch.netやFC2 Wikiなどのサイト内リンクからクッションページを削除して直接飛ぶようにするスクリプトです。
-// @auther        PC-CNT
-// @updateURL     https://raw.githubusercontent.com/PC-CNT/UserscriptWorks/main/plsDirectJump/plsDirectJump.js
-// @include       /^https?:\/\/*
-// @grant         none
+// @name            plsDirectJump
+// @namespace       https://github.com/PC-CNT/UserscriptWorks/
+// @version         0.0.1
+// @description:ja  2ch.netやFC2 Wikiなどのサイト内リンクからクッションページを削除して直接飛ぶようにするスクリプトです。
+// @auther          PC-CNT
+// @updateURL       https://raw.githubusercontent.com/PC-CNT/UserscriptWorks/main/plsDirectJump/plsDirectJump.js
+// @include         /^https?:\/\/*
+// @grant           none
 // ==/UserScript==
 
 
 
 
 (function () {
-    // if (location.hostname.match(/^(2|5)ch\.net$/)) {
-    //     // Do something
-    //     let class_thread = document.getElementsByClassName("thread");
-    //     class_thread.querySelectorAll("a").forEach(function(value) {
-    //         console.log(value.getAttribute("href"));
-    //     });
-    // }
-    console.log("test")
+    if (location.hostname.match(/^(2|5)ch\.net$/)) {
+        // Do something
+        let class_thread = document.getElementsByClassName("thread");
+        class_thread.querySelectorAll("a").forEach(function(value) {
+            console.log(value.getAttribute("href"));
+        });
+    }
+    // console.log("test")
 })();
--- searchengine2KZ_BRAIN_Mobile/searchengine2KZ_BRAIN_Mobile.user.js
+++ searchengine2KZ_BRAIN_Mobile/searchengine2KZ_BRAIN_Mobile.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name           Search engine to KZ BRAIN Mobile
-// @namespace      https://github.com/PC-CNT/UserscriptWorks/
-// @version        0.0.5
-// @author         PC-CNT
-// @description    検索エンジンの結果から直接KZ BRAIN Mobileの軽量サイトに直接移動できるスクリプトです。
-// @updateURL      https://github.com/PC-CNT/UserscriptWorks/raw/main/searchengine2KZ_BRAIN_Mobile/searchengine2KZ_BRAIN_Mobile.user.js
-// @include        /^(http|https):\/\/www\.google\..+\/search.*/
-// @grant          none
+// @name            Search engine to KZ BRAIN Mobile
+// @namespace       https://github.com/PC-CNT/UserscriptWorks/
+// @version         0.0.5
+// @author          PC-CNT
+// @description:ja  検索エンジンの結果から直接KZ BRAIN Mobileの軽量サイトに直接移動できるスクリプトです。
+// @updateURL       https://github.com/PC-CNT/UserscriptWorks/raw/main/searchengine2KZ_BRAIN_Mobile/searchengine2KZ_BRAIN_Mobile.user.js
+// @include         /^(http|https):\/\/www\.google\..+\/search.*/
+// @grant           none
 // ==/UserScript==
 
 (function () {
