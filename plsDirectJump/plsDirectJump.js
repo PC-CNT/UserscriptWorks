44bd3fb7c311dd7667b3ca29f2720d5d1c8a8cc8
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2021-11-16 12:36:55 +0900

    Update plsDirectJump.js

--- plsDirectJump/plsDirectJump.js
+++ plsDirectJump/plsDirectJump.js
@@ -5,7 +5,7 @@
 // @description   2ch.netやFC2 Wikiなどのサイト内リンクからクッションページを削除して直接飛ぶようにするスクリプトです。
 // @auther        PC-CNT
 // @updateURL     
-// @match         https?:\/\/[A-Za-z0-9].*(2|5)ch\.net\/[A-Za-z0-9]
+// @match         *
 
 
 
@@ -16,8 +16,11 @@
 
 
 (function () {
-    // Check if the page is a 2ch.net or 5ch.net page.
     if (location.hostname.match(/^(2|5)ch\.net$/)) {
         // Do something
+        let class_thread = document.getElementsByClassName("thread");
+        class_thread.querySelectorAll("a").forEach(function(value) {
+            console.log(value.getAttribute("href"));
+        });
     }
 })();
