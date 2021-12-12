e3ffb45da84d349cd113c3d57ebcc90ac91a0e37
gm_fox <49097489+PC-CNT@users.noreply.github.com>
2021-12-13 06:06:08 +0900

    [add] debug

--- plsDirectJump/plsDirectJump.user.js
+++ plsDirectJump/plsDirectJump.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            plsDirectJump
 // @namespace       https://github.com/PC-CNT/UserscriptWorks/
-// @version         0.1.7
+// @version         0.2.0
 // @description     This is a script (planned) to remove cushion pages such as 2ch.net and FC2 Wiki from <a href> so that you can jump directly to them.
 // @description:ja  <a href>から2ch.netやFC2 Wikiなどのクッションページを削除して直接飛ぶようにするスクリプト（の予定）です。
 // @author          PC-CNT
@@ -17,6 +17,9 @@
 
 ( () => {
     console.log("===START UserscriptWorks/plsDirectJump===");
+
+    const flag_debug = false;
+
     function http_https(host) {
         if (host.match("https")) {
             return "https";
@@ -33,95 +36,54 @@
         }
     }
 
+    //* 配列の先頭をタイトルとしたconsole.groupの表示
+    function debug(msglist) {
+        if (!flag_debug) {
+            return;
+        }
+        console.group(msglist[0]);
+        msglist.slice(1).forEach(msg => {
+            console.info(msg);
+        });
+        console.groupEnd();
+    }
+
     document.querySelectorAll("a").forEach(function(value) {
-        //! url_source = value.getAttribute("href");
-        let url_source = value.href;
-        // console.log(url_source);
+        let url_source_abs = value.href;
+        let url_source_rel = value.getAttribute("href")
         //TODO: hrefの編集
-        if (url_source) {
-            // console.log("<a href> found:" + url_source);
-            if (url_source.match(/^https?:\/\/jump.(2|5)ch\.net\/\?.*/)) {
+        if (url_source_abs) {
+            if (url_source_abs.match(/^https?:\/\/jump.(2|5)ch\.net\/\?.*/)) {
                 //* 2ちゃんねる
                 //* (jump.5ch.net/?http://example.com/example.html, jump.2ch.net/?,)
-                // console.log("2ch-match:" + url_source);
-                value.setAttribute("href", fixPrefix(url_source.replace(/^https?:\/\/jump.(2|5)ch\.net\/\?/, "")));
-                // url_replace(value, url_source, /^https?:\/\/jump.(2|5)ch\.net\/\?/, "");
-                // console.log("URL changed:" + value.getAttribute("href"));
+                let _2ch = ["match:2ch.net", "絶対ﾊﾟｽ：" + url_source_abs];
+                value.setAttribute("href", fixPrefix(url_source_abs.replace(/^https?:\/\/jump.(2|5)ch\.net\/\?/, "")));
+                _2ch.push("変更後ﾊﾟｽ：" + value.href);
+                debug(_2ch);
             }
-            // if (url_source.match(/^https?:\/\/.+\.wiki\.fc2\.com\/jump\//)) {
+            // if (url_source_abs.match(/^https?:\/\/.+\.wiki\.fc2\.com\/jump\//)) {
             if (location.hostname.match(/^.+\.wiki\.fc2\.com/)) {
                 //* FC2 Wiki
                 //* (https://example.wiki.fc2.com/jump/https/example.com%2exapmple)
-                if (url_source.match(/^\/jump\/https?\/.*/)) {
-                    // console.log(decodeURIComponent(url_source.replace(/^\/jump\/https?\//, http_https(url_source) + "://")));
-                    value.setAttribute("href", decodeURIComponent(url_source.replace(/^\/jump\/https?\//, http_https(url_source) + "://")));
+                let _fc2 = ["match:FC2 Wiki", "絶対ﾊﾟｽ：" + url_source_abs, "相対ﾊﾟｽ：" + url_source_rel];
+                if (url_source_rel.match(/^\/jump\/https?\/.*/)) {
+                    value.setAttribute("href", decodeURIComponent(url_source_rel.replace(/^\/jump\/https?\//, http_https(url_source_abs) + "://")));
                     value.setAttribute("target", "_blank");
                     value.setAttribute("rel", "noopener noreferrer");
+                    _fc2.push("変更後ﾊﾟｽ：" + value.href);
+                    debug(_fc2);
                 }
             }
-            if (url_source.match(/^https?:\/\/kakaku\.com\/jump\/\?url=.*/)) {
-                //* 価格.COM
+            if (url_source_abs.match(/^https?:\/\/kakaku\.com\/jump\/\?url=.*/)) {
+                //* 価格.com
                 //* (https://kakaku.com/jump/?url=https%3A%2F%2Fwww%2Eexample%2Ecom%2F)
-                value.setAttribute("href", decodeURIComponent(url_source.replace(/^https?:\/\/kakaku\.com\/jump\/\?url=/, "")));
-                //? console.log("URL changed:" + value.getAttribute("href"));
+                let _kakaku = ["match:価格.com", "絶対ﾊﾟｽ：" + url_source_abs];
+                value.setAttribute("href", decodeURIComponent(url_source_abs.replace(/^https?:\/\/kakaku\.com\/jump\/\?url=/, "")));
+                _kakaku.push("変更後ﾊﾟｽ：" + value.href);
+                debug(_kakaku);
             }
         }
-
     });
 
-    // if (location.hostname.match(/^www\.youtube\.com/)) {
-        //* youtube
-        //* https://www.youtube.com/redirect?q=https%3A%2F%2Fexample.com%2F
-
-        // const observer_config = {
-        //     childList: true,
-        //     subtree: true
-        // };
-
-        // let id_comments = document.getElementById("comments");
-        // let comment_section = document.getElementsByTagName("ytd-comment-thread-renderer")
-        // console.log("DirectJump->Youtube")
-        // let observer = new MutationObserver(function(mutations) {
-        //     observer.disconnect();
-        //     mutations.forEach( node => {
-        //         console.log("MutationObserver: " + node.target.innerHTML);
-        //         value.classList.remove("yt-simple-endpoint")
-        //         value.setAttribute("href", value.textContent);
-        //     });
-        //     observer.observe(comment_section, observer_config);
-        // });
-
-
-        // if (document.getElementById("comments") != null) {
-        // if (url_source == null) {
-            // value.classList.remove("yt-simple-endpoint")
-            // value.setAttribute("href", value.textContent);
-        // }
-        // }
-        // observer.observe(comment_section, observer_config);
-    //     window.addEventListener('DOMContentLoaded', function(e){
-    //         console.log("DOMContentLoaded");
-    //         const jsInitCheckTimer = setInterval(jsLoaded, 1000);
-    //         function jsLoaded() {
-    //             if (document.querySelector("#comments") != null) {
-    //                 clearInterval(jsInitCheckTimer);
-    //                 document.querySelectorAll("ytd-comment-thread-renderer").forEach(function(value) {
-    //                     value.querySelectorAll("a").forEach(function(a_tag_main) {
-    //                         // let a_tag_main = value.querySelector("a");
-    //                         let url_source = a_tag_main.getAttribute("href")
-    //                         if (!a_tag_main.getAttribute("href")) {
-    //                             a_tag_main.setAttribute("href", url_source);
-    //                             a_tag_main.setAttribute("target", "_blank");
-    //                             a_tag_main.setAttribute("rel", "noopener noreferrer");
-    //                             // console.log(url_source);
-    //                         }
-    //                     });
-    //                 });
-    //             }
-    //         }
-    //     });
-    // }
-
-    // console.log("test")
     console.log("===END UserscriptWorks/plsDirectJump===");
 })();
