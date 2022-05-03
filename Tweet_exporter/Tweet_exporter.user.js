e65a37310281f14b7f409bb0d5c3c689ea4da780
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2022-05-03 17:57:22 +0900

    Update Tweet_exporter.user.js

--- Tweet_exporter/Tweet_exporter.user.js
+++ Tweet_exporter/Tweet_exporter.user.js
@@ -218,8 +218,8 @@ TODO: 引リツがバグる
                 return;
             } else {
                 group.classList.add("tweet-exporter-added");
-                let _sharemenu_div = group.lastElementChild;
-                let _export_div = _sharemenu_div.cloneNode(true);
+                const _sharemenu_div = group.lastElementChild;
+                const _export_div = _sharemenu_div.cloneNode(true);
                 _export_div.querySelector("svg").innerHTML = `<g transform="rotate(180, 12, 8.4)"><path d="M17.53 7.47l-5-5c-.293-.293-.768-.293-1.06 0l-5 5c-.294.293-.294.768 0 1.06s.767.294 1.06 0l3.72-3.72V15c0 .414.336.75.75.75s.75-.336.75-.75V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22c.293-.293.293-.767 0-1.06z"></path></g><g><path d="M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z"></path></g>`
                 // _export_div.querySelector("g").setAttribute("transform", "rotate(180, 12, 8.4)");
                 // _export_div.setAttribute("tabindex", "0");
