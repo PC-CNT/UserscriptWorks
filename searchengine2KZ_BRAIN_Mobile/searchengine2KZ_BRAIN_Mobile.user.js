594ab48581a2ff1637bdb0d0ae5a83e7ab29e5dd
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2023-02-17 00:20:47 +0900

    Update searchengine2KZ_BRAIN_Mobile.user.js
    別にしてくれと言われたので

--- searchengine2KZ_BRAIN_Mobile/searchengine2KZ_BRAIN_Mobile.user.js
+++ searchengine2KZ_BRAIN_Mobile/searchengine2KZ_BRAIN_Mobile.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Search engine to KZ BRAIN Mobile
 // @namespace       https://github.com/PC-CNT/UserscriptWorks/
-// @version         0.0.7
+// @version         0.1
 // @author          PC-CNT
 // @license         MIT
 // @description     This script allows you to open the KZ BRAIN Mobile lightweight site directly from search engine results in a separate tab.
@@ -9,25 +9,28 @@
 // @downloadURL     https://github.com/PC-CNT/UserscriptWorks/raw/main/searchengine2KZ_BRAIN_Mobile/searchengine2KZ_BRAIN_Mobile.user.js
 // @updateURL       https://github.com/PC-CNT/UserscriptWorks/raw/main/searchengine2KZ_BRAIN_Mobile/searchengine2KZ_BRAIN_Mobile.user.js
 // @supportURL      https://github.com/PC-CNT/UserscriptWorks/issues
-// @match           http*://www.google.com/search?*
+// @match           https://www.google.com/search?*
 // @grant           none
 // ==/UserScript==
 
-(function () {
+(function() {
     console.log("===START UserscriptWorks/searchengine2KZ_BRAIN_Mobile===");
     const KZ_url = "http://servermobile.net/index.php?_kzm_u=";
 
-    const div_id_search = document.getElementById("search");
-    div_id_search.querySelectorAll(".g").forEach(function(value) {
-        value.querySelectorAll("a").forEach(function(a_tag_main) {
-            // let a_tag_main = value.querySelector("a");
-            const url_source = a_tag_main.getAttribute("href");
-            a_tag_main.setAttribute("href", KZ_url + url_source);
-            a_tag_main.setAttribute("target", "_blank");
-            a_tag_main.setAttribute("rel", "noopener noreferrer");
-            // console.log(url_source);
-            // 検索結果の数だけ繰り返す
+
+    if (location.href.match(/^https:\/\/www.google.com\/search\?.+/)) {
+        //* Google.com
+
+        document.querySelector("#search").querySelectorAll(".g").forEach(function(block) {
+            //? `[data-sokoban-container]`
+            block.querySelectorAll(`.yuRUbf`).forEach((c) => {
+                let link = c.querySelector(`a`).href;
+                let tab_html = document.createElement("div");
+                tab_html.innerHTML = `<a href="${KZ_url}${link}" target="_blank">KZ BRAIN Mobile</a>`
+                c.insertAdjacentElement("beforeend", tab_html);
+            })
         });
-    });
+    }
+
     console.log("===END UserscriptWorks/searchengine2KZ_BRAIN_Mobile===");
 })();
