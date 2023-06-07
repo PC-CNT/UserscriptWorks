f1663220721990d25cb3fab4a30ba875d83c3362
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2023-06-08 01:33:49 +0900

    Update plsDirectJump.user.js
    ・デバッグ（もどき）消した
    ・zhihu.comに対応

--- plsDirectJump/plsDirectJump.user.js
+++ plsDirectJump/plsDirectJump.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            plsDirectJump
 // @namespace       https://github.com/PC-CNT/UserscriptWorks/
-// @version         0.6.2
+// @version         0.7
 // @description     This is a script (planned) to remove cushion pages such as 2ch.net and FC2 Wiki from <a href> so that you can jump directly to them.
 // @description:ja  <a href>から2ch.netやFC2 Wikiなどのクッションページを削除して直接飛ぶようにするスクリプト（の予定）です。
 // @author          PC-CNT
@@ -10,18 +10,14 @@
 // @updateURL       https://raw.githubusercontent.com/PC-CNT/UserscriptWorks/main/plsDirectJump/plsDirectJump.user.js
 // @supportURL      https://github.com/PC-CNT/UserscriptWorks/issues
 // @match           *://*/*
-// @match           https://www.youtube.com/watch?v=*
-// @match           https://steamcommunity.com/*
+// @match           *://www.youtube.com/watch?v=*
+// @match           *://steamcommunity.com/*
 // @grant           none
 // ==/UserScript==
 
 
 ( () => {
     "use strict";
-
-    // console.log("===START UserscriptWorks/plsDirectJump===");
-
-    const flag_debug = false;
 
     function fixPrefix(url) {
         if (url.match(/^https?:\/\//)) {
@@ -37,18 +33,6 @@
         } else {
             return false;
         }
-    }
-
-    //* 配列の先頭をタイトルとしたconsole.groupの表示
-    function debug(msglist) {
-        if (!flag_debug) {
-            return;
-        }
-        console.group(msglist[0]);
-        msglist.slice(1).forEach(msg => {
-            console.info(msg);
-        });
-        console.groupEnd();
     }
 
 
@@ -74,15 +58,12 @@
                     block.querySelectorAll("a").forEach(function(value) {
                         const a_text = value.innerText;
                         if (isURL(a_text)) {
-                            console.info("value:", value);
-                            const _youtube = ["match:Youtube"];
-                            _youtube.push("絶対ﾊﾟｽ：" + a_text);
+                            // console.info("value:", value);
                             value.classList.remove("yt-simple-endpoint");
                             value.style.textDecoration = "none";
                             value.setAttribute("href", a_text);
                             value.setAttribute("target", "_blank");
                             value.setAttribute("rel", "noopener noreferrer");
-                            debug(_youtube);
                         }
                     });
                 });
@@ -92,7 +73,7 @@
                 if (ra.href.match(/https:\/\/www\.youtube\.com\/redirect\?.+/)) {
                     // https://www.youtube.com/redirect?event=video_description&redir_token=rrrrr&q=https%3A%2F%2Fexample.com%2F&v=RgFaK6ZQifE
                     ra.href = decodeURIComponent(ra.href.match(/&q=([^&]+)/)[1]);
-                    console.log(ra.href)
+                    // console.log(ra.href)
                 }
             })
         });
@@ -109,10 +90,7 @@
                 if (value.href.match(/^https:\/\/okwave\.jp\/jump\?url=.+/)) {
                     // * OKWAVE
                     // * (https://okwave.jp/jump?url=https%3A%2F%2Fexample.com%2F)
-                    const _okwave = ["match:OKWAVE", "絶対ﾊﾟｽ：" + value.href];
                     value.setAttribute("href", decodeURIComponent(value.href.replace(/^https:\/\/okwave\.jp\/jump\?url=/, "")));
-                    _okwave.push("変更後ﾊﾟｽ：" + value.href);
-                    debug(_okwave);
                 } else {
                     observer.disconnect();
                 }
@@ -134,12 +112,9 @@
             const al = document.querySelectorAll(`a`);
             al.forEach((value) => {
                 if (value.href.match(/^https:\/\/game\.nicovideo.jp\/atsumaru\/jump\?(link_in=gamepage&)?url=/)) {
-                    const _atsumaru = ["match:ゲームアツマール", "絶対ﾊﾟｽ：" + value.href];
                     const _clone = value.cloneNode(true);
                     _clone.setAttribute("href", decodeURIComponent(value.href.replace(/^https:\/\/game\.nicovideo.jp\/atsumaru\/jump\?(link_in=gamepage&)?url=/, "")));
                     value.parentNode.replaceChild(_clone, value);
-                    _atsumaru.push("変更後ﾊﾟｽ：" + _clone.href);
-                    debug(_atsumaru);
                 }
             });
             // observer.disconnect();
@@ -158,11 +133,8 @@
             const al = document.querySelectorAll(`a`);
             al.forEach((value) => {
                 if (value.href.match(/^https?:\/\/.+\.2chan\.net\/bin\/jump\.php\?/)) {
-                    const _futaba = ["match:ふたば☆ちゃんねる", "絶対ﾊﾟｽ：" + value.href];
                     value.setAttribute("href", decodeURIComponent(value.href.replace(/^https?:\/\/.+\.2chan\.net\/bin\/jump\.php\?/, "")));
                     value.setAttribute("rel", "noopener noreferrer");
-                    _futaba.push("変更後ﾊﾟｽ：" + value.href);
-                    debug(_futaba);
                 }
             });
         });
@@ -179,49 +151,40 @@
                 if (url_source_abs.match(/^https?:\/\/jump.(2|5)ch\.net\/\?.*/)) {
                     //* 2ちゃんねる
                     //* (https://jump.5ch.net/?http://example.com/, jump.2ch.net/?,)
-                    const _2ch = ["match:2ch.net", "絶対ﾊﾟｽ：" + url_source_abs];
                     value.setAttribute("href", fixPrefix(url_source_abs.replace(/^https?:\/\/jump.(2|5)ch\.net\/\?/, "")));
-                    _2ch.push("変更後ﾊﾟｽ：" + value.href);
-                    debug(_2ch);
                 }
                 // if (url_source_abs.match(/^https?:\/\/.+\.wiki\.fc2\.com\/jump\//)) {
                 if (location.hostname.match(/^.+\.wiki\.fc2\.com/)) {
                     //* FC2 Wiki
                     //* (https://example.wiki.fc2.com/jump/https/example.com%2exapmple)
-                    const _fc2 = ["match:FC2 Wiki", "絶対ﾊﾟｽ：" + url_source_abs, "相対ﾊﾟｽ：" + url_source_rel];
                     if (url_source_rel.match(/^\/jump\/https?\/.*/)) {
                         value.setAttribute("href", decodeURIComponent(url_source_rel.replace(/^\/jump\/(https?)\//, "$1://")));
                         value.setAttribute("target", "_blank");
                         value.setAttribute("rel", "noopener noreferrer");
-                        _fc2.push("変更後ﾊﾟｽ：" + value.href);
-                        debug(_fc2);
                     }
                 }
                 if (url_source_abs.match(/^https?:\/\/kakaku\.com\/jump\/\?url=.*/)) {
                     //* 価格.com
                     //* (https://kakaku.com/jump/?url=https%3A%2F%2Fwww%2Eexample%2Ecom%2F)
-                    const _kakaku = ["match:価格.com", "絶対ﾊﾟｽ：" + url_source_abs];
                     value.setAttribute("href", decodeURIComponent(url_source_abs.replace(/^https?:\/\/kakaku\.com\/jump\/\?url=/, "")));
-                    _kakaku.push("変更後ﾊﾟｽ：" + value.href);
-                    debug(_kakaku);
                 }
                 if (url_source_abs.match(/^https:\/\/steamcommunity\.com\/linkfilter\/\?url=/)) {
                     //* Steam
                     //* (https://steamcommunity.com/linkfilter/?url=https://example.com/)
-                    const _steam = ["match:Steam", "絶対ﾊﾟｽ：" + url_source_abs];
                     value.setAttribute("href", decodeURIComponent(url_source_abs.replace(/^https:\/\/steamcommunity\.com\/linkfilter\/\?url=/, "")));
-                    _steam.push("変更後ﾊﾟｽ：" + value.href);
-                    debug(_steam);
                 }
                 if (url_source_abs.match(/^https:\/\/re\.wikiwiki\.jp\//)) {
                     //* Wikiwiki
                     //* (https://re.wikiwiki.jp/?http://example.com/)
-                    const _wikiwiki = ["match:Wikiwiki", "絶対ﾊﾟｽ：" + url_source_abs];
                     value.setAttribute("href", decodeURIComponent(url_source_abs.replace(/^https:\/\/re\.wikiwiki\.jp\/\?/, "")));
                     value.setAttribute("target", "_blank");
                     value.setAttribute("rel", "noopener noreferrer");
-                    _wikiwiki.push("変更後ﾊﾟｽ：" + value.href);
-                    debug(_wikiwiki);
+                }
+                if (url_source_abs.match(/^https?:\/\/link\.zhihu\.com\/\?target=.+/)) {
+                    // * 知乎
+                    // * (https://link.zhihu.com/?target=https%3A//example.com/)
+                    value.setAttribute("href", decodeURIComponent(url_source_abs.replace(/^https?:\/\/link\.zhihu\.com\/\?target=/, "")));
+                    value.setAttribute("target", "_blank");
                 }
             }
         });
@@ -239,5 +202,4 @@
         others();
     }
 
-    // console.log("===END UserscriptWorks/plsDirectJump===");
 })();
