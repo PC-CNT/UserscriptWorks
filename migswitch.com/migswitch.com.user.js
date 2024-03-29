3043b4bf9ed28ac9c609f16a3ac32781314bbf3d
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2024-03-29 17:55:20 +0900

    Create migswitch.com.user.js

---
+++ migswitch.com/migswitch.com.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name            I agree Migswitch.com
+// @namespace       https://github.com/PC-CNT/UserscriptWorks/
+// @version         2024-03-29
+// @description     
+// @author          PC-CNT
+// @license         MIT
+// @match           *://migswitch.com/*
+// @icon            
+// ==/UserScript==
+
+window.addEventListener('load', () => {
+    if (document.querySelector(`label[for="acceptTOS"]`)) {
+        if (!document.querySelector(`input[type=checkbox][class="mr-3"]`).checked) {
+            document.querySelector(`input[type=checkbox][class="mr-3"]`).click();
+        }
+        document.querySelector(`button`).click();
+    }
+});
