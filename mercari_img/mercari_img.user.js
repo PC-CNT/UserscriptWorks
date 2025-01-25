e8715993465ff27cd4f8342c4e82470d43c8f888
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2025-01-25 23:51:12 +0900

    Create mercari_img.user.js

---
+++ mercari_img/mercari_img.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name            メルカリの商品画像落とすやつ
+// @namespace       https://github.com/PC-CNT/UserscriptWorks/
+// @version         2025.01.25.1950
+// @description     
+// @author          PC-CNT
+// @license         MIT
+// @match           *://*.mercari.com/*
+// @icon            https://web-jp-assets-v2.mercdn.net/_next/static/media/favicon-192.f25eb78a.jpg
+// @grant           GM_registerMenuCommand
+// @require         https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.54/dist/zip-full.min.js
+// @require         https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js
+// ==/UserScript==
+
+
+(() => {
+    "use strict";
+
+    async function createzip(params) {
+        // let parent_div = document.querySelector(`[data-testid="vertical-thumbnail-scroll"]`) | document.querySelector(`[data-testid="carousel"]`)
+        let parent_div = document.querySelector(`[data-testid="vertical-thumbnail-scroll"]`);
+        
+        let zipWriter = new zip.ZipWriter(new zip.BlobWriter("application/zip"));
+
+        for (let slide of parent_div.querySelectorAll(`div.slick-slide`)) await (
+            zipWriter.add(slide.querySelector(`img`).src.match(/\/([^\/]+)(?:\?\w+)$/)[1], new zip.HttpReader(slide.querySelector(`img`).src))
+        )
+        // await Promise.all([
+        //     parent_div.querySelectorAll(`div[class="slick-slide"]`).forEach(slide => {
+        //         let filename = slide.querySelector(`img`).src.match(/\/([^\/]+)(?:\?\w+)$/)[1];
+        //         zipWriter.add(filename, new zip.HttpReader(slide.querySelector(`img`).src));
+        //     })
+        // ]);
+        return zipWriter.close();
+    }
+
+    GM_registerMenuCommand("zipでダウンロード", function() {
+        createzip().then(function(blob) {
+            // console.log(blob);
+            let id = location.href.match(/^https?:\/\/(?:.+\.)?mercari\.com\/item\/([a-zA-Z0-9]+)/)[1];
+            saveAs(blob, `${document.querySelector("h1").innerText}[${id}].zip`);
+        });
+    })
+})();
