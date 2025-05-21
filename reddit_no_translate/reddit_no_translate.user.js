94a5fa16801e2751ba3e8ae80247339e50331641
PC-CNT <49097489+PC-CNT@users.noreply.github.com>
2025-05-21 11:44:38 +0900

    Create reddit_no_translate.user.js

---
+++ reddit_no_translate/reddit_no_translate.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name            Redditの翻訳を無効化するだけ
+// @namespace       https://github.com/PC-CNT/UserscriptWorks/
+// @version         2025.05.21.0954
+// @description     
+// @author          PC-CNT
+// @license         MIT
+// @match           *://*/*
+// @icon            https://www.google.com/s2/favicons?domain=reddit.com&sz=128
+// ==/UserScript==
+
+(() => {
+    "use strict";
+
+    const r = /https:\/\/\w+\.reddit\.com\/r\/\w+\/comments\/\w+(\/.+)\/\?tl=\w+/
+
+    document.querySelectorAll("a").forEach(a => {
+        if (a.href.match(r)) {
+            a.href = a.href.replace(/\?tl=\w+/, "");
+        }
+    })
+
+    if (location.href.match(r)) {
+        location.href = location.href.replace(/\?tl=\w+/, "")
+    }
+})()
